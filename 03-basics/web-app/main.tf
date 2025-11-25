8e6b2b528b78e3e6d52c478562998ca724aec868
KenanRicky <kaburaricky@gmail.com>
2025-11-25 15:58:06 +0400

    modified changes

--- 03-basics/web-app/main.tf
+++ 03-basics/web-app/main.tf
@@ -1,18 +1,19 @@
+############################################################
+# TERRAFORM BACKEND (S3 + DynamoDB)
+############################################################
 terraform {
-  # Assumes s3 bucket and dynamo DB table already set up
-  # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "devops-directive-tf-state34"
     key            = "03-basics/web-app/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-locking"
+    dynamodb_table = "terraform-state-rickyID"
     encrypt        = true
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
@@ -21,28 +22,91 @@ provider "aws" {
   region = "us-east-1"
 }
 
+############################################################
+# VPC + SUBNETS
+############################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+############################################################
+# SECURITY GROUPS
+############################################################
+resource "aws_security_group" "instances" {
+  name   = "instance-security-group"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "instances_http" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instances.id
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "alb" {
+  name   = "alb-security-group"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "alb_http" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "alb_egress" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+############################################################
+# EC2 INSTANCES
+############################################################
 resource "aws_instance" "instance_1" {
-  ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 1" > index.html
-              python3 -m http.server 8080 &
-              EOF
+  ami           = "ami-011899242bb902164"
+  instance_type = "t3.micro"
+  security_groups = [
+    aws_security_group.instances.name
+  ]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World 1" > index.html
+    python3 -m http.server 8080 &
+  EOF
 }
 
 resource "aws_instance" "instance_2" {
-  ami             = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instances.name]
-  user_data       = <<-EOF
-              #!/bin/bash
-              echo "Hello, World 2" > index.html
-              python3 -m http.server 8080 &
-              EOF
+  ami           = "ami-011899242bb902164"
+  instance_type = "t3.micro"
+  security_groups = [
+    aws_security_group.instances.name
+  ]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World 2" > index.html
+    python3 -m http.server 8080 &
+  EOF
 }
 
+############################################################
+# S3 BUCKET FOR APP STORAGE
+############################################################
 resource "aws_s3_bucket" "bucket" {
   bucket_prefix = "devops-directive-web-app-data"
   force_destroy = true
@@ -50,13 +114,15 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.bucket.id
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_crypto_conf" {
-  bucket = aws_s3_bucket.bucket.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  bucket = aws_s3_bucket.bucket.id
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -64,52 +130,21 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_crypto_con
   }
 }
 
-data "aws_vpc" "default_vpc" {
-  default = true
+############################################################
+# LOAD BALANCER (ALB)
+############################################################
+resource "aws_lb" "alb" {
+  name               = "web-app-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
 }
 
-data "aws_subnet_ids" "default_subnet" {
-  vpc_id = data.aws_vpc.default_vpc.id
-}
-
-resource "aws_security_group" "instances" {
-  name = "instance-security-group"
-}
-
-resource "aws_security_group_rule" "allow_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.instances.id
-
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.load_balancer.arn
-
-  port = 80
-
-  protocol = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-resource "aws_lb_target_group" "instances" {
+resource "aws_lb_target_group" "tg" {
   name     = "example-target-group"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default_vpc.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
     path                = "/"
@@ -122,70 +157,32 @@ resource "aws_lb_target_group" "instances" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "instance_1" {
-  target_group_arn = aws_lb_target_group.instances.arn
+resource "aws_lb_target_group_attachment" "tg_attach_1" {
+  target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.instance_1.id
   port             = 8080
 }
 
-resource "aws_lb_target_group_attachment" "instance_2" {
-  target_group_arn = aws_lb_target_group.instances.arn
+resource "aws_lb_target_group_attachment" "tg_attach_2" {
+  target_group_arn = aws_lb_target_group.tg.arn
   target_id        = aws_instance.instance_2.id
   port             = 8080
 }
 
-resource "aws_lb_listener_rule" "instances" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.instances.arn
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
 
-
-resource "aws_security_group" "alb" {
-  name = "alb-security-group"
-}
-
-resource "aws_security_group_rule" "allow_alb_http_inbound" {
-  type              = "ingress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-resource "aws_security_group_rule" "allow_alb_all_outbound" {
-  type              = "egress"
-  security_group_id = aws_security_group.alb.id
-
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-}
-
-
-resource "aws_lb" "load_balancer" {
-  name               = "web-app-lb"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default_subnet.ids
-  security_groups    = [aws_security_group.alb.id]
-
-}
-
+############################################################
+# ROUTE53 HOSTED ZONE + A RECORD
+############################################################
 resource "aws_route53_zone" "primary" {
   name = "devopsdeployed.com"
 }
@@ -196,25 +193,23 @@ resource "aws_route53_record" "root" {
   type    = "A"
 
   alias {
-    name                   = aws_lb.load_balancer.dns_name
-    zone_id                = aws_lb.load_balancer.zone_id
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
     evaluate_target_health = true
   }
 }
 
-resource "aws_db_instance" "db_instance" {
-  allocated_storage = 20
-  # This allows any minor version within the major engine_version
-  # defined below, but will also result in allowing AWS to auto
-  # upgrade the minor version of your DB. This may be too risky
-  # in a real production environment.
-  auto_minor_version_upgrade = true
-  storage_type               = "standard"
-  engine                     = "postgres"
-  engine_version             = "12"
-  instance_class             = "db.t2.micro"
-  name                       = "mydb"
-  username                   = "foo"
-  password                   = "foobarbaz"
-  skip_final_snapshot        = true
+############################################################
+# RDS (FIXED: VALID ENGINE + INSTANCE CLASS)
+############################################################
+resource "aws_db_instance" "db" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "14.10"
+  instance_class       = "db.t3.micro"
+  
+  name                = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  skip_final_snapshot  = true
 }
